995be9dad5c8b69ebe06ed8ae7c36c865f7567a2
soonSoonyy <soonyy13@gmail.com>
2023-09-16 12:14:28 +0900

    컨테이너 위젯 실습

---
+++ flutter_app_test/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text('Study to Container'),
+        ),
+        body: Center(child: CustomContainer())),
+  ));
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+      decoration: BoxDecoration(
+          color: Color(0xFFD8EEC0),
+          image: const DecorationImage(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+            fit: BoxFit.cover,
+          ),
+          border: Border.all(
+            color: Color(0xFFB1D28A),
+            style: BorderStyle.solid,
+            width: 8,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(6, 6),
+                blurRadius: 10,
+                spreadRadius: 2),
+            BoxShadow(
+                color: Colors.blue.withOpacity(0.3),
+                offset: Offset(-6, -6),
+                blurRadius: 10,
+                spreadRadius: 2)
+          ]),
+      child: Center(
+          child: Container(
+              color: Color(0xFFC4C1CB), child: Text('Hello, Container!'))),
+    );
+  }
+}
