186682a8d38ff6c80a0e0b8baa826fdf42e6caf3
soonyy13 <soonyy13@gmail.com>
2024-01-06 12:49:21 +0900

    노드 스터디 1일차 셋팅 및 request response 처리 기본 1

---
+++ nodeServerVimeo/app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+
+app.listen(3000, function(){
+		console.log("start server...");
+});
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/public/main.html")
+})
+
+
+app.get('/main', function (req, res) {
+	res.sendFile(__dirname + "/public/main.html")
+})
+
+app.post('/email-post', function (req, res) {
+	console.log(req.body)
+	res.render('email.ejs',{'email':req.body.email})
+})
