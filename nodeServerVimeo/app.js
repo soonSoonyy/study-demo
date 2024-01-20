f8bf94ab4c62a237cb1419d83812f0e8e4e0f6fc
soonSoonyy <soonyy13@gmail.com>
2024-01-20 14:47:18 +0900

    노드 스터디 라우터 처리

--- nodeServerVimeo/app.js
+++ nodeServerVimeo/app.js
@@ -1,15 +1,7 @@
 var express = require('express')
 var app = express()
-var bodyParser = require('body-parser')
-var mariadb = require('mariadb');
-
-const pool = mariadb.createPool({
-	host: 'localhost',
-	user:'root',
-	password: 'gpgp2932',
-	database : 'nodeservervimeodb',
-	connectionLimit: 5
-});
+var bodyParser = require('body-parser');
+var router = require('./router/index') // ./router/index.js
 
 
 app.listen(3000, function(){
@@ -19,49 +11,13 @@ app.listen(3000, function(){
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + "/public/main.html")
-})
+
+app.use('/', router)
 
 
-app.get('/main', function (req, res) {
-	res.sendFile(__dirname + "/public/main.html")
-})
 
-app.post('/email-post', function (req, res) {
-	console.log(req.body)
-	res.render('email.ejs',{'email':req.body.email})
-})
-app.post('/ajax-send-email', function (req, res) {
-	var reqEmail = req.body.email;
-	var responseData = {};
 
-	var findEmailQuery= pool.getConnection()
-						.then(conn => {
-							conn.query("SELECT * FROM USERS WHERE EMAIL = ? LIMIT 1", [reqEmail])
-								.then((rows) => {
-									if(rows.length > 0){
-										responseData.code = 200;
-										responseData.result = "ok";
-										responseData.data = rows[0]
-									}else{
-										responseData.code = 404;
-										responseData.result = "error";
-										responseData.message = "등록 되지 않은 이메일 입니다. 회원가입을 해주세요";
-									}
-									conn.end();
-									res.json(responseData);
 
-								})
-								.catch(err => {
-									conn.end();
-									throw err;
-								})
-						})
-						.catch(err => {
-							throw err;
-						})
-
-})
